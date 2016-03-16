23d8242f51a507ca4ae4143139f5c86fc9a30d7d
AntoineB <antoine.baran@gmail.com>
2016-03-16 15:21:58 +0100

    Boundary + Edge
    Classe boundary et edge

--- src/Boundary.java
+++ src/Boundary.java
@@ -1,4 +1,83 @@
 
 public class Boundary {
+	
 	private String name;
+	private boolean boundary;
+	// coordinates:
+	int x;
+	int y;
+	
+	/*
+	 * Constructors:
+	 */
+	public Boundary(String name){
+		this.name = name;
+		boundary = true;
+		x = 0;
+		y = 0;
+	}
+	
+	
+	public Boundary(String name, boolean boundary){
+		this.name = name;
+		this.boundary = boundary;
+		x = 0;
+		y = 0;
+	}
+	
+	public Boundary(String name, boolean boundary, int x, int y){
+		this.name = name;
+		this.boundary = boundary;
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	
+	/*
+	 * Accessors:
+	 */
+	
+	public String getName () {
+		return name;
+	}
+	
+	public boolean getBoundary () {
+		return boundary;
+	}
+	
+	public int getX () {
+		return x;
+	}
+
+	public int getY () {
+		return y;
+	}
+
+	public int [] getCoordinates () {
+		return new int [] {x,y};
+	}
+	
+	/*
+	 * Mutators:
+	 */
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public void setBoundary (Boolean boundary) {
+		this.boundary = boundary;
+	}
+
+	public void setX (int x) {
+		this.x = x;
+	}
+
+	public void setY (int y) {
+		this.y = y;
+	}
+
+	public void setCoordinates () {
+		
+	}
 }
--- src/Edge.java
+++ src/Edge.java
@@ -1,4 +1,49 @@
 
 public class Edge {
 	private String name;
+	private String target;
+	private String source;
+	
+	/*
+	 * Constructors:
+	 */
+	
+	public Edge (String name, String target, String source) {
+		this.name = name;
+		this.target = target;
+		this.source = source;
+	}
+	
+	/*
+	 * Accessors:
+	 */
+	
+	public String getName () {
+		return name;
+	}
+	
+	public String getTarget () {
+		return target;
+	}
+	
+	public String getSource () {
+		return source;
+	}
+	
+	/*
+	 * Mutators:
+	 */
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public void setTarget (String target) {
+		this.target = target;
+	}
+	
+	public void setSource (String source) {
+		this.source = source;
+	}
+	
 }
